8d005f115fb1ecfdf30b2affa695950268dbc4ad
Fiza Rasheed <fizazainab10@gmail.com>
2024-05-31 18:04:47 +0500

    become an investor first step

---
+++ Elements/become_investor_page1.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Initialize the WebDriver (e.g., Chrome)
+driver = webdriver.Chrome()
+
+# Open a webpage
+driver.get('https://investsage.com/password/reset')
+
+join_investsage_title = driver.find_element(By.XPATH, '//*[@id="step1"]/div/h4')
+full_name_label =  driver.find_element(By.XPATH, '//*[@id="form_step1"]/div[1]/div[1]/div/label') 
+full_name_input = driver.find_element(By.XPATH,'//*[@id="name"]')
+email_label = driver.find_element(By.XPATH,'//*[@id="form_step1"]/div[1]/div[2]/div/label')
+email_input = driver.find_element(By.XPATH,'//*[@id="email"]')
+password_label = driver.find_element(By.XPATH,'//*[@id="form_step1"]/div[1]/div[3]/div/label')
+password_input =  driver.find_element(By.XPATH,'//*[@id="password"]')
+confirm_password_label = driver.find_element(By.XPATH,'//*[@id="confirm-pass"]/label')
+confirm_password_input = driver.find_element(By.XPATH,'//*[@id="confirm_password"]')
+go_back_button = driver.find_element(By.XPATH,'//*[@id="form_step1"]/div[2]/a')
+continue_to_next_step_button = driver.find_element(By.XPATH,'//*[@id="registerSubmit"]')
+
--- Elements/login_page.py
+++ Elements/login_page.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Initialize the WebDriver (e.g., Chrome)
 driver = webdriver.Chrome()
@@ -10,9 +13,9 @@ driver.get('https://investsage.com')
 # Locate an element using XPathc
 login_title = driver.find_element(By.XPATH, "/html/body/div[2]/section/div/div/div[1]/div[2]/div/h4")
 email_label = driver.find_element(By.XPATH, '//*[@id="user-login"]/div[1]/label')
-email_input = driver.find_element(By.XPATH, '//*[@id="email"]')
+email_input = driver.find_element(By.XPATH, '//*[@id="email"]').send_keys("myemail@example.com")
 password_label = driver.find_element(By.XPATH,'//*[@id="user-login"]/div[2]/label')
-password_input = driver.find_element(By.XPATH,'//*[@id="password"]')
+password_input = driver.find_element(By.XPATH,'//*[@id="password"]').send_keys("mypassword")
 select_user_type_text = driver.find_element(By.XPATH,'//*[@id="user-login"]/div[3]/label')
 investor_radio_button = driver.find_element(By.XPATH, '//*[@id="investor_radio"]')
 investor_radio_button_title = driver.find_element(By.XPATH, '//*[@id="max_funding_limit_per_user"]/div[1]/label')
@@ -20,9 +23,12 @@ vendor_radio_button = driver.find_element(By.XPATH, '//*[@id="vendor_radio"]')
 vendor_radio_button_title = driver.find_element(By.XPATH, '//*[@id="max_funding_limit_per_user"]/div[2]/label')
 forgot_Password_link = driver.find_element(By.XPATH, '//*[@id="user-login"]/div[4]/a')
 forgot_Password_icon= driver.find_element(By.XPATH,'//*[@id="user-login"]/div[4]/a/i')
-lets_go_button= driver.find_element(By.XPATH,'//*[@id="user-login"]/button')
+lets_go_button= driver.find_element(By.XPATH,'//*[@id="user-login"]/button').click()
 become_an_investor_button= driver.find_element(By.XPATH,'//*[@id="user-login"]/div[5]/a[2]')
 become_a_vendor_button= driver.find_element(By.XPATH,'//*[@id="user-login"]/div[5]/a[1]')
+
+
+
 
 
 
